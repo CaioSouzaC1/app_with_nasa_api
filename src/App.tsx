3043fc6cf8a1447a7563369b33e92a3880621f6d
CaioNotebook <caiocsouza2010@gmail.com>
2023-04-01 13:16:50 -0300

    continuing front

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import styles from "./styles.module.css";
 
 function App() {
   interface NasaData {
+    copyright: string;
+    date: string;
+    explanation: string;
     title: string;
-    description: string;
-    imageUrl: string;
+    url: string;
   }
   const [nasaData, setNasaData] = useState<NasaData | false>(false);
   useEffect(() => {
@@ -15,13 +17,14 @@ function App() {
 
   const get_initial_data = async () => {
     try {
-      setNasaData(
-        await (
-          await fetch(
-            "https://api.nasa.gov/planetary/apod?api_key=wF1ZEl8L97PGCgDAszfosqPBCBrIWhCCBxCb5jbI"
-          )
-        ).json()
-      );
+      const response = await (
+        await fetch(
+          "https://api.nasa.gov/planetary/apod?api_key=wF1ZEl8L97PGCgDAszfosqPBCBrIWhCCBxCb5jbI"
+        )
+      ).json();
+      console.log(response);
+
+      setNasaData(response);
     } catch (error) {
       console.log(error);
     }
@@ -31,15 +34,11 @@ function App() {
       <h1>TSApod</h1>
       {nasaData && (
         <>
-          <h1>{nasaData.title}</h1>
-          <p>{nasaData.description}</p>
-          <img src={nasaData.imageUrl} alt={nasaData.title} />
+          <h2>{nasaData.title}</h2>
+          <p>{nasaData.explanation}</p>
+          <Img img_alt={nasaData.title} source={nasaData.url} />{" "}
         </>
       )}
-      <Img
-        img_alt="Alt Text"
-        source="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-      ></Img>
     </main>
   );
 }
